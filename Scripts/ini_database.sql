821ee243acc65d4c3831ba3c3b4e4a9d32caea56
Eman Lotfy <eman.lotfy16485@gmail.com>
2025-10-19 11:49:27 +0300

    Create ini_database.sql

---
+++ Scripts/ini_database.sql
@@ -0,0 +1,15 @@
+Use master;
+Go
+----create database
+create Database DataWarehouse;
+Go
+
+use DataWarehouse;
+Go
+
+  -----create schema
+create SCHEMA Bronze;
+GO
+create SCHEMA Silver;
+GO
+create SCHEMA Gold;
